d2ee231127e347f01cc9d9c0d292cdd14e9d3146
Pasindu Perera <pasinduanuradhaperera@gmail.com>
2023-04-10 20:15:51 +0530

    navBar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import React, {useState} from 'react'
+import {AiOutlineClose, AiOutlineMenu} from 'react-icons/ai';
 
 const Navbar = () => {
+
+  const [nav, setNav] = useState(false);
+  const handleNav =() =>{
+    setNav(!nav);
+  }
   return (
-    <div className='text-white'>
-        <h1>PASINDU</h1>
+    <div className=' flex justify-between items-center h-24 max-w-[1240px] mx-auto px-4 text-white'>
+        <h1 className='w-full text-3xl font-bold text-white'>PASINDU</h1>
+        <ul className='flex hidden'>
+          <li className='p-4 '>Home</li>
+          <li className='p-4 '>About</li>
+          <li className='p-4 '>Projects</li>
+          <li className='p-4 '>Contact</li>
+        </ul>
+        <div>
+          <AiOutlineMenu size={30} />
+        </div>
+        <div className='fixed left-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-[#000300]'>
+        <h1 className='w-full text-3xl font-bold text-white m-4'>PASINDU</h1>
+
+        <ul className='uppercase p-4'>
+          <li className='p-4 border-b border-gray-600'>Home</li>
+          <li className='p-4 border-b border-gray-600'>About</li>
+          <li className='p-4 border-b border-gray-600'>Projects</li>
+          <li className='p-4 '>Contact</li>
+        </ul>
+        </div>
     </div>
   )
 }
