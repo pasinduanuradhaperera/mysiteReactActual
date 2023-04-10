149ebff6aea65f7df9691afdb21e955076eefb62
Pasindu Perera <pasinduanuradhaperera@gmail.com>
2023-04-10 20:43:54 +0530

    nav animation

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -16,11 +16,11 @@ const Navbar = () => {
           <li className='p-4 '>Projects</li>
           <li className='p-4 '>Contact</li>
         </ul>
-        <div>
-          <AiOutlineMenu size={30} />
+        <div onClick={handleNav}>
+          {!nav? <AiOutlineClose size={30}/>:<AiOutlineMenu size={30}/>}          
         </div>
-        <div className='fixed left-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-[#000300]'>
-        <h1 className='w-full text-3xl font-bold text-white m-4'>PASINDU</h1>
+        <div className={!nav? "fixed left-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-[#000300] ease-in-out duration-500":"fixed left-[-100%] "}>
+        <h1 className='w-full text-3xl font-bold text-white mx-4 mt-8'>PASINDU</h1>
 
         <ul className='uppercase p-4'>
           <li className='p-4 border-b border-gray-600'>Home</li>
