509f0768adbdf7f0ab4f8dab56da5da4163537d4
Pasindu Perera <pasinduanuradhaperera@gmail.com>
2023-04-10 20:52:15 +0530

    nav finished

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,23 +10,23 @@ const Navbar = () => {
   return (
     <div className=' flex justify-between items-center h-24 max-w-[1240px] mx-auto px-4 text-white'>
         <h1 className='w-full text-3xl font-bold text-white'>PASINDU</h1>
-        <ul className='flex hidden'>
-          <li className='p-4 '>Home</li>
-          <li className='p-4 '>About</li>
-          <li className='p-4 '>Projects</li>
-          <li className='p-4 '>Contact</li>
+        <ul className='hidden md:flex'>
+          <li className='p-4 font-bold'>Home</li>
+          <li className='p-4 font-bold'>About</li>
+          <li className='p-4 font-bold'>Projects</li>
+          <li className='p-4 font-bold'>Contact</li>
         </ul>
-        <div onClick={handleNav}>
+        <div onClick={handleNav} className='block md:hidden'>
           {!nav? <AiOutlineClose size={30}/>:<AiOutlineMenu size={30}/>}          
         </div>
         <div className={!nav? "fixed left-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-[#000300] ease-in-out duration-500":"fixed left-[-100%] "}>
         <h1 className='w-full text-3xl font-bold text-white mx-4 mt-8'>PASINDU</h1>
 
         <ul className='uppercase p-4'>
-          <li className='p-4 border-b border-gray-600'>Home</li>
-          <li className='p-4 border-b border-gray-600'>About</li>
-          <li className='p-4 border-b border-gray-600'>Projects</li>
-          <li className='p-4 '>Contact</li>
+          <li className='p-4 font-bold border-b border-gray-600'>Home</li>
+          <li className='p-4 font-bold border-b border-gray-600'>About</li>
+          <li className='p-4 font-bold border-b border-gray-600'>Projects</li>
+          <li className='p-4 font-bold'>Contact</li>
         </ul>
         </div>
     </div>
