d241e3b852d2761e86b013ee98521f28c059be63
Pasindu Perera <pasinduanuradhaperera@gmail.com>
2023-04-13 22:11:40 +0530

    remove back

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import Navbar from "./components/Navbar";
 import Hero from './components/Hero';
+import pic from './assets/star.jpg'
+import blur from './assets/overlay.png'
 
 
 function App() {
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,24 +1,36 @@
 import React from 'react';
 import Typed from 'react-typed';
+import pic from '../assets/me.jpeg'
 const Hero = () => {
-  return (
-    <div className='text-white'>
-        <div className='mx-w-[800px] mt-[-96px] w-full h-screen mx-auto text-center flex flex-col justify-center'>
-                <h1 className='md:text-7xl sm:text-6xl text-5xl  text-[#32f8ff]'>Hi, There.</h1>
-                <div className='flex justify-center items-center'>
-                    <h1 className='md:text-6xl sm:text-5xl text-4xl'>I am a&nbsp; </h1>
-                    <Typed 
-                        className='md:text-6xl sm:text-5xl text-4xl text-[#32f8ff]'
-                        strings={['Programmer', 'Freelancer', 'Designer']} 
-                        typeSpeed={120} 
-                        backSpeed={140}
-                        loop
+    return (
+        <div>
+            <div className='text-white md:flex justify-center items-center mx-w-[800px]  w-full h-screen mx-auto text-center mt-[-96px] align-middle' >
+
+                <div className='flex items-center justify-center md:pt-0  pt-[96px] '>
+                    <div className='rounded-3xl overflow-hidden'>
+                         <img src={pic} alt="" className='w-[300px]'/>
+                    </div>
+                </div>
+                <div className=' flex flex-col justify-center md:min-w-[600px]  min-w-[360px]'>
+                    <h1 className='md:text-7xl sm:text-6xl text-5xl  text-[#32f8ff]'>Hi, There.</h1>
+                    <div className='flex justify-center items-center'>
+                        <h1 className='md:text-6xl sm:text-5xl text-4xl'>I am a&nbsp; </h1>
+                        <Typed
+                            className='md:text-6xl sm:text-5xl text-4xl text-[#32f8ff]'
+                            strings={['Programmer', 'Freelancer', 'Designer']}
+                            typeSpeed={120}
+                            backSpeed={140}
+                            loop
                         />
+                    </div>
+
                 </div>
 
+            </div>
+
         </div>
-    </div>
-  )
+
+    )
 }
 
 export default Hero
